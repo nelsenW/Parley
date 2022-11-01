9e4dc7ba991b1565307503beedc3a52e63621b52
nelsenw <wnelsen9@gmail.com>
2022-11-01 16:47:00 -0700

    action_cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,11 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
+  # Mount Action Cable outside main process or domain.
+  config.action_cable.mount_path = nil
+  config.action_cable.url = "ws://localhost:3000/cable"
+  config.action_cable.allowed_request_origins = [ "http://localhost:3000", /http:\/\/localhost:3000.*/ ]
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
