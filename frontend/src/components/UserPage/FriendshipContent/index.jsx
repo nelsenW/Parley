2f91f71b2e68459201c976c4a1958df26b096140
nelsenw <wnelsen9@gmail.com>
2022-12-01 16:37:34 -0800

    dms styking

--- frontend/src/components/UserPage/FriendshipContent/index.jsx
+++ frontend/src/components/UserPage/FriendshipContent/index.jsx
@@ -1,81 +1,105 @@
-import { useDispatch, useSelector } from 'react-redux';
-import './friendshipContent.css';
-import { useRef, useEffect } from 'react';
-import Message from '../../Messages';
-import MessageForm from '../../Messages/messageForm';
-import consumer from '../../../consumer';
-import { showFriendship } from '../../../store/friendships';
-import { receiveDM, removeDM } from '../../../store/dms';
+import { useDispatch, useSelector } from "react-redux";
+import "./friendshipContent.css";
+import { useRef, useEffect } from "react";
+import Message from "../../Messages";
+import MessageForm from "../../Messages/messageForm";
+import consumer from "../../../consumer";
+import { showFriendship } from "../../../store/friendships";
+import { receiveDM, removeDM } from "../../../store/dms";
 
 export default function FriendshipContent({
-	friendship,
-	wumpusClass,
-	wumpusText
+  friendship,
+  wumpusClass,
+  wumpusText,
 }) {
-	const dispatch = useDispatch();
-	const prevId = useRef();
-	let dms = useSelector((state) => (state.dms ? Object.values(state.dms) : [{text: 'No Messages Yet'}]));
-	let subscription;
-	const sessionUser = useSelector((state) => state.session.currentUser.id);
+  const dispatch = useDispatch();
+  const prevId = useRef();
+  let dms = useSelector((state) =>
+    state.dms ? Object.values(state.dms) : [{ text: "No Messages Yet" }]
+  );
+  let subscription;
+  const sessionUser = useSelector((state) => state.session.currentUser.id);
 
-	const enterFriendship = () => {
-		subscription = consumer.subscriptions.create(
-			{ channel: 'FriendshipsChannel', id: friendship.id },
-			{
-				received: ({ type, dm, user, id }) => {
-					switch (type) {
-						case 'RECEIVE_USER':
-							break;
-						case 'REMOVE_USER':
-							break;
-						case 'RECEIVE_DM':
-							dispatch(receiveDM(dm));
-							break;
-						case 'DESTROY_DM':
-							dispatch(removeDM(id));
-							break;
-						default:
-							console.log('Unhandled broadcast: ', type);
-							break;
-					}
-				}
-			}
-		);
-	};
+  const enterFriendship = () => {
+    subscription = consumer.subscriptions.create(
+      { channel: "FriendshipsChannel", id: friendship.id },
+      {
+        received: ({ type, dm, user, id }) => {
+          switch (type) {
+            case "RECEIVE_USER":
+              break;
+            case "REMOVE_USER":
+              break;
+            case "RECEIVE_DM":
+              dispatch(receiveDM(dm));
+              break;
+            case "DESTROY_DM":
+              dispatch(removeDM(id));
+              break;
+            default:
+              console.log("Unhandled broadcast: ", type);
+              break;
+          }
+        },
+      }
+    );
+  };
 
-	useEffect(() => {
-		if (friendship?.id) {
-			dispatch(showFriendship(friendship.id));
-		}
-		if (friendship?.id && friendship.id !== prevId) {
-			prevId.current = friendship.id;
-			subscription?.unsubscribe();
-			enterFriendship();
-		}
-		return () => {
-			subscription?.unsubscribe();
-		};
-	}, [friendship, dms.length]);
+  useEffect(() => {
+    if (friendship?.id) {
+      dispatch(showFriendship(friendship.id));
+    }
+    if (friendship?.id && friendship.id !== prevId) {
+      prevId.current = friendship.id;
+      subscription?.unsubscribe();
+      enterFriendship();
+    }
+    return () => {
+      subscription?.unsubscribe();
+    };
+  }, [friendship, dms.length]);
 
-	return friendship ? (
-		<div className='center-column'>
-			<ul className='friendship-dms'>
-				{dms.map((dm) => {
-					let modify = false;
-					if (dm.userId === sessionUser) {
-						modify = true;
-					}
-					return <Message {...dm} modify={modify} />;
-				})}
-			</ul>
-			<MessageForm friendship={friendship} />
-		</div>
-	) : (
-		<div className='center-column'>
-			<div id='list'>
-				<div className={wumpusClass}></div>
-				<p>{wumpusText}</p>
-			</div>
-		</div>
-	);
+  return friendship ? (
+    <div className="center-column">
+      <ul className="friendship-dms">
+        {dms?.length > 0 ? (
+          dms.map((dm) => {
+            let modify = false;
+            if (dm.userId === sessionUser) {
+              modify = true;
+            }
+            return <Message {...dm} modify={modify} />;
+          })
+        ) : (
+          <div id="no-message-card">
+            {friendship.user.photo ? (
+              <img
+                src={friendship.user.photo}
+                style={{ backgroundColor: "transparent" }}
+                className="dm-userIcon"
+              />
+            ) : (
+              <i
+                className="fa-solid fa-skull-crossbones dm-userIcon"
+                style={{ backgroundColor: `${friendship.user.color}` }}
+              ></i>
+            )}
+			<h1>{friendship.user.username}</h1>
+            <p>
+              This is the beginning of your direct message history with @
+              {friendship.user.username}
+            </p>
+          </div>
+        )}
+      </ul>
+      <MessageForm friendship={friendship} />
+    </div>
+  ) : (
+    <div className="center-column">
+      <div id="list">
+        <div className={wumpusClass}></div>
+        <p>{wumpusText}</p>
+      </div>
+    </div>
+  );
 }
